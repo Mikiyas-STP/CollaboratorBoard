0b69b10cdef7b367a58ede238891572b9809e1ac
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-10-16 22:42:46 +0100

    starter server code, ready for future logic

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+
+// This array will store the history of all drawing actions
+let actionHistory = [];
+
+// HTTP endpoint for new clients to get the full history
+app.get("/history", (req, res) => {
+  res.json(actionHistory);
+});
+
+io.on("connection", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
